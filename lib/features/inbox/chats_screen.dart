dbdd9c35e2d0122b6445c2b806543e3135be98eb
Ryan <jiunson@naver.com>
2023-11-09 09:51:37 +0900

    #11.3 AnimatedList part Two

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone_2nd/constants/sizes.dart';
+import 'package:tiktok_clone_2nd/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -14,14 +15,44 @@ class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
   final List<int> _items = [];
 
+  final Duration _duration = const Duration(milliseconds: 300);
+
   void _addItem() {
     if (_key.currentState != null) {
+      // UI에 item 추가
       _key.currentState!.insertItem(
         _items.length,
-        duration: const Duration(milliseconds: 500),
+        duration: _duration,
       );
+      // item 저장
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      // UI에 item 삭제
+      _key.currentState!.removeItem(
+          index,
+          (context, animation) => SizeTransition(
+                sizeFactor: animation,
+                child: Container(
+                  color: Colors.red,
+                  child: _makeTile(index),
+                ),
+              ),
+          duration: _duration);
+    }
+    // item 삭제
+    _items.removeAt(index);
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
   }
 
   @override
@@ -44,41 +75,47 @@ class _ChatsScreenState extends State<ChatsScreen> {
         ),
         itemBuilder: (context, index, animation) {
           return FadeTransition(
-            key: UniqueKey(),
+            key: Key('$index'), // UniqueKey(),
             opacity: animation,
             child: SizeTransition(
               sizeFactor: animation,
-              child: ListTile(
-                leading: const CircleAvatar(
-                  radius: 30,
-                  foregroundImage: NetworkImage(
-                    "https://avatars.githubusercontent.com/u/3612017",
-                  ),
-                  child: Text('니꼬'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      "Lynn",
-                      style: TextStyle(fontWeight: FontWeight.w600),
-                    ),
-                    Text(
-                      "2:16 PM",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: const Text("Don't forget to make video"),
-              ),
+              child: _makeTile(index),
             ),
           );
         },
       ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+          "https://avatars.githubusercontent.com/u/3612017",
+        ),
+        child: Text('니꼬'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Text(
+            "Lynn",
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text("Don't forget to make video"),
     );
   }
 }
