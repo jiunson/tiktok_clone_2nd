7cf542e519f2692b5802491eca65e2ceee4c758b
Ryan <jiunson@naver.com>
2023-11-13 17:02:51 +0900

    #13.2 showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -12,6 +12,47 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              debugPrint('$date');
+
+              if (!context.mounted) {
+                return; // buildcotexts across async gaps 오류 해결.
+              }
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              debugPrint('$time');
+
+              if (!context.mounted) {
+                return; // buildcontexts across async gaps 오류 해결.
+              }
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              debugPrint('$booking');
+            },
+            title: const Text("What is your birthday?"),
+          ),
+          ListTile(
             onTap: () => showAboutDialog(
                 context: context,
                 applicationVersion: "1.0",
