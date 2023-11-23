4a59bf4a705ded0dc36c3c35a3fa1c0a3f38ba15
Ryan <jiunson@naver.com>
2023-11-23 11:19:43 +0900

    #19.4 Flash Mode

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -14,7 +14,7 @@ class VideoRecordingScreen extends StatefulWidget {
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   bool _hasPermission = false;
   bool _isSelfieMode = false;
-
+  late FlashMode _flashMode;
   late CameraController _cameraController;
 
   Future<void> initCamera() async {
@@ -23,11 +23,13 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
 
     if (cameras.isEmpty) return;
 
-    // 카메라 컨트롤러 초기화, 0-후방카메라, 1-전방카메라
+    // 카메라 컨트롤러 생성, 0-후방카메라, 1-전방카메라
     _cameraController = CameraController(
         cameras[_isSelfieMode ? 1 : 0], ResolutionPreset.ultraHigh);
 
-    await _cameraController.initialize();
+    await _cameraController.initialize(); // 컨트롤러 초기화
+
+    _flashMode = _cameraController.value.flashMode; // flashMode 변수 초기화
   }
 
   Future<void> initPermissions() async {
@@ -67,6 +69,12 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     setState(() {});
   }
 
+  Future<void> _setFlashMode(FlashMode newFlashMode) async {
+    await _cameraController.setFlashMode(newFlashMode);
+    _flashMode = newFlashMode;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,10 +106,54 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                   Positioned(
                     top: Sizes.size20,
                     left: Sizes.size20,
-                    child: IconButton(
-                      color: Colors.white,
-                      onPressed: _toggleSelfieMode,
-                      icon: const Icon(Icons.cameraswitch),
+                    child: Column(
+                      children: [
+                        IconButton(
+                          color: Colors.white,
+                          onPressed: _toggleSelfieMode,
+                          icon: const Icon(Icons.cameraswitch),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.off
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.off),
+                          icon: const Icon(
+                            Icons.flash_off_rounded,
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.always
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.always),
+                          icon: const Icon(
+                            Icons.flash_on_rounded,
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.auto
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.auto),
+                          icon: const Icon(
+                            Icons.flash_auto_rounded,
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashMode == FlashMode.torch
+                              ? Colors.amber.shade200
+                              : Colors.white,
+                          onPressed: () => _setFlashMode(FlashMode.torch),
+                          icon: const Icon(
+                            Icons.flashlight_on_rounded,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
